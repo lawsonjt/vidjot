5f7e8c52df71a422de314fe14d282a852ac7df2a
unknown <josh@jumbomega.com>
2018-12-02 11:42:35 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.Promise = global.Promise;
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+  useNewUrlParser: true
+})
+  .then(() => console.log('MongoDb Connected...'))
+  .catch(err => console.log(err));
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// Index route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
