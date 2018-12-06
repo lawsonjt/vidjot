deb3141129c3f0e736b9814c3101c6d9b2165705
Joshua <jlawson@realpeoplesratings.com>
2018-12-05 22:35:39 -0600

    edit Ideas

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
+
+// Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
 //Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev', {
-  useNewUrlParser: true
+    useNewUrlParser: true
 })
-  .then(() => console.log('MongoDb Connected...'))
-  .catch(err => console.log(err));
+        .then(() => console.log('MongoDb Connected...'))
+        .catch(err => console.log(err));
 
 // Load Idea Model
 require('./models/Idea');
@@ -24,50 +27,85 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Method override middleware
+app.use(methodOverride('_method'));
+
 // Index route
 app.get('/', (req, res) => {
-  const title = 'Welcome';
-  res.render('index', {
-    title: title
-  });
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
 });
 
 // About Route
 app.get('/about', (req, res) => {
-  res.render('about');
+    res.render('about');
+});
+
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+        .sort({ date: 'desc' })
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas: ideas
+            });
+        });
 });
 
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
-  res.render('ideas/add');
+    res.render('ideas/add');
+});
+
+// Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({ _id: req.params.id })
+        .then(idea => {
+            res.render('ideas/edit', {
+                idea: idea
+            });
+        });
 });
 
 // Process form
 app.post('/ideas', (req, res) => {
-  let errors = [];
+    let errors = [];
 
-  if (!req.body.title) {
-    errors.push({ text: 'Please add a title' });
-  }
+    if (!req.body.title) {
+        errors.push({ text: 'Please add a title' });
+    }
 
-  if (!req.body.details) {
-    errors.push({ text: 'Please add some details' });
-  }
+    if (!req.body.details) {
+        errors.push({ text: 'Please add some details' });
+    }
 
-  if(errors.length > 0){
-    res.render('ideas/add', {
-      errors: errors,
-      title: req.body.title,
-      details: req.body.details
-    });
-  } else {
-    res.send('passed');
-  }
-
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        };
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            });
+    }
 });
 
+// Edit Form process
+app.put('/ideas/:id', (req, res) => {
+    res.send('PUT');
+});
 
 const port = 5000;
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+    console.log(`Server started on port ${port}`);
 });
